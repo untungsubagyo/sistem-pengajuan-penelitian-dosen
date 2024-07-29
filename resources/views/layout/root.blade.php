5ed8dc051aa37e91351b1834939d32c449772d27
maulana adam <wandgenius6@gmail.com>
2024-07-29 15:00:18 +0700

    test

--- resources/views/layout/root.blade.php
+++ resources/views/layout/root.blade.php
@@ -3,7 +3,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>@yield('title-page', 'Sistem Pengejuan Penelitian')</title>
+   <title>@yield('title-page', 'Sistem Pengajuan Penelitian')</title>
 </head>
 <body>
    @yield('content')
