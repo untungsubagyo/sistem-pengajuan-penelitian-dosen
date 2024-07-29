6a5456e238a012fe4bb0438d175fbf16190e09f1
maulana adam <wandgenius6@gmail.com>
2024-07-29 09:25:21 +0700

    views folder structuring, and adding login controller

---
+++ resources/views/pages/auth/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.root')
+
+@section('title-page')
+    Login
+@endsection
+
+@section('content')
+    <h1>Login Page</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'index'])->name('login');
