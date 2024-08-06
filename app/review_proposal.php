102d8edff1e4237105e98c70c064a2f2c4d60834
SyifaZahra <zahraassyifa2006@gmail.com>
2024-08-06 13:24:50 +0700

    pengumuman and proposal

---
+++ app/review_proposal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class review_proposal extends Model
+{
+    use HasFactory;
+}
