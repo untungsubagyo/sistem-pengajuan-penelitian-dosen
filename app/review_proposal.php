84293dc840009b47caca6049fcbf877b96e8ec57
SyifaZahra <zahraassyifa2006@gmail.com>
2024-08-06 13:58:12 +0700

    wss

--- app/review_proposal.php
+++ app/review_proposal.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class review_proposal extends Model
 {
     use HasFactory;
+    protected $fillable = [
+    'review_data', 'file', 'catatan', 'skor_rumusan_masalah', 'skor_luaran', 'skor_metode', 'skor_tinjauan_pustaka', 'skor_kelayakan', 'status'
+    ];
 }
