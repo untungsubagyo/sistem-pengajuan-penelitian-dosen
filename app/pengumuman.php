817ba8cacc2432349d4fc643f2a116e279513b2a
SyifaZahra <zahraassyifa2006@gmail.com>
2024-08-06 13:24:50 +0700

    pengumuman and proposal

---
+++ app/pengumuman.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pengumuman extends Model
+{
+    use HasFactory;
+}
