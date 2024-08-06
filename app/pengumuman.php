8a3fb84e0c2fc6f1b54f432215dd676679187795
SyifaZahra <zahraassyifa2006@gmail.com>
2024-08-06 13:57:56 +0700

    model ppengu,uman and review

--- app/pengumuman.php
+++ app/pengumuman.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class pengumuman extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'tanggal', 'judul', 'deskripsi', 'file', 'status'
+    ]
 }
