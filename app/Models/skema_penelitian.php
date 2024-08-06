8652e8e9495cee173ff102a2a4200a3d90aa78a4
malikah <siswiian@gmail.com>
2024-08-06 00:16:23 -0600

    membuat model migrasii skema

--- app/Models/skema_penelitian.php
+++ app/Models/skema_penelitian.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class skema_penelitian extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nama_skema',
+        'sumber_dana',
+        'durasi',
+        'satuan_durasi',
+        'total_biaya',
+        'bobot_rumusan_masalah',
+        'bobot_luaran',
+        'bobot_metode',
+        'bobot_tinjauan_pustaka',
+        'bobot_kelayakan',
+    ];
 }
