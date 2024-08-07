519faf2c686c1845788978af1d12066e27d4eccd
maulana adam <wandgenius6@gmail.com>
2024-08-07 12:23:18 +0700

    fixing migration

--- database/migrations/2024_08_07_045717_create_review_proposals_table.php
+++ database/migrations/2024_08_07_045717_create_review_proposals_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('review_proposals', function (Blueprint $table) {
             $table->id();
-            $table->string('id_user',18);
+            $table->string('id_user',10);
             $table->foreign('id_user')->references('nidn')->on('users')->onDelete('cascade');
-            $table->bigInteger('id_proposal');
+            $table->bigInteger('id_proposal')->unsigned();
             $table->foreign('id_proposal')->references('id')->on('proposals')->onDelete('cascade');
             $table->date ('reviwer_data');
             $table->text ('file');
